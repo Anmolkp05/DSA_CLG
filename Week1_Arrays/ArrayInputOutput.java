5a0808ddf52c1768ed760ac7bd3f70c28ff2956f
DSA <anmolkamalapuri@gmail.com>
2025-07-23 02:26:18 +0530

    first Commit

---
+++ Week1_Arrays/ArrayInputOutput.java
@@ -0,0 +1,26 @@
+package DSA.Week1_Arrays;
+
+import java.util.Scanner;
+
+public class ArrayInputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Elements in the array are:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
